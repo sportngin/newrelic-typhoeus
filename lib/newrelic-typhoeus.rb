3c05f48da8186b8119058913955333de4e1cec68
Nate Greene <nate.greene@tstmedia.com>
2013-05-15 12:19:42 -0500

    fixed a bug

--- lib/newrelic-typhoeus.rb
+++ lib/newrelic-typhoeus.rb
@@ -15,23 +15,22 @@ module Newrelic
 
       executes do
         ::Typhoeus::Request.instance_eval do
-          class << self
-            %w[post get put delete options head patch].each do |http_method|
-              if respond_to?(http_method)
-                define_method "#{http_method}_with_newrelic_trace" do |*args|
-                  uri = URI.parse(args.first)
-                  metrics = ["External/#{uri.host}/Typhoeus/#{http_method.capitalize}","External/#{uri.host}/all"]
-                  if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
-                    metrics << "External/allWeb"
-                  else
-                    metrics << "External/allOther"
-                  end
-                  self.class.trace_execution_scoped metrics do
-                    send("#{http_method}_without_newrelic_trace", *args)
-                  end
+          %w[post get put delete options head patch].each do |http_method|
+            if respond_to?(http_method)
+              define_singleton_method "#{http_method}_with_newrelic_trace" do |*args|
+                uri = URI.parse(args.first)
+                metrics = ["External/#{uri.host}/Typhoeus/#{http_method.capitalize}","External/#{uri.host}/all"]
+                if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+                  metrics << "External/allWeb"
+                else
+                  metrics << "External/allOther"
                 end
-                alias_method_chain "#{http_method}", :newrelic_trace
+                self.class.trace_execution_scoped metrics do
+                  send("#{http_method}_without_newrelic_trace", *args)
+                end
               end
+              eigenclass = class << self; self; end
+              eigenclass.alias_method_chain "#{http_method}", :newrelic_trace
             end
           end
         end
