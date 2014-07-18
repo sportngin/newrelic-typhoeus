d81ea8f4d693dad24e2c935834ad2227c660bfe2
Nate Greene <nate.greene@sportngin.com>
2014-07-18 13:46:32 -0500

    added support for other quick methods

--- lib/newrelic-typhoeus.rb
+++ lib/newrelic-typhoeus.rb
@@ -14,23 +14,25 @@ module Newrelic
       end
 
       executes do
-        ::Typhoeus::Request.instance_eval do
-          %w[post get put delete options head patch].each do |http_method|
-            if respond_to?(http_method)
-              define_singleton_method "#{http_method}_with_newrelic_trace" do |*args|
-                uri = URI.parse(args.first)
-                metrics = ["External/#{uri.host}/Typhoeus/#{http_method.capitalize}","External/#{uri.host}/all"]
-                if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
-                  metrics << "External/allWeb"
-                else
-                  metrics << "External/allOther"
+        [::Typhoeus, ::Typhoeus::Request].each do |typhoeus|
+          typhoeus.instance_eval do
+            %w[post get put delete options head patch].each do |http_method|
+              if respond_to?(http_method)
+                define_singleton_method "#{http_method}_with_newrelic_trace" do |*args|
+                  uri = URI.parse(args.first)
+                  metrics = ["External/#{uri.host}/Typhoeus/#{http_method.capitalize}","External/#{uri.host}/all"]
+                  if NewRelic::Agent::Instrumentation::MetricFrame.recording_web_transaction?
+                    metrics << "External/allWeb"
+                  else
+                    metrics << "External/allOther"
+                  end
+                  self.class.trace_execution_scoped metrics do
+                    send("#{http_method}_without_newrelic_trace", *args)
+                  end
                 end
-                self.class.trace_execution_scoped metrics do
-                  send("#{http_method}_without_newrelic_trace", *args)
-                end
+                eigenclass = class << self; self; end
+                eigenclass.alias_method_chain "#{http_method}", :newrelic_trace
               end
-              eigenclass = class << self; self; end
-              eigenclass.alias_method_chain "#{http_method}", :newrelic_trace
             end
           end
         end
